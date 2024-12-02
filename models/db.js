b01e0ce53827d398219b44f7e6defa5e71b4c80c
Victor Oliveira <victormanoliv@gmail.com>
2024-12-02 14:36:01 -0300

    3000

--- models/db.js
+++ models/db.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize ( process.env.B_banco, /*'blogbushtres',*/ process.env.B_mysqlUser, /*'root',*/
-    process.env.B_passMy, /*'victorluiza',*/ {
+const sequelize = new Sequelize ( process.env.B_banco,  process.env.B_mysqlUser,
+    process.env.B_passMy,  {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3307
+    port: 3306 || 3307
 })
 
 module.exports = {
