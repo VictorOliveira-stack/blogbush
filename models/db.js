544a0f560a7799724e48989d9fb839531a348e1a
Victor Oliveira <victormanoliv@gmail.com>
2024-12-03 02:07:22 -0300

    -3307

--- models/db.js
+++ models/db.js
@@ -38,7 +38,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL || {
     username: process.env.MYSQLUSER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQLPORT || 3307,
+    port: process.env.MYSQLPORT,
     ssl:true,
     dialectOptions: {
         ssl:{
