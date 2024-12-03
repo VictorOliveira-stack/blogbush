3f7488cf16bd4f05bfba3c93303edd16e6210a77
Victor Oliveira <victormanoliv@gmail.com>
2024-12-02 23:53:37 -0300

    database url

--- models/db.js
+++ models/db.js
@@ -1,15 +1,29 @@
-require('dotenv').config()
+/*require('dotenv').config()
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize ( process.env.MYSQL_DATABASE,  process.env.MYSQLUSER,
     process.env.MYSQL_ROOT_PASSWORD,  {
-    host: 'localhost',
+    host: process.env.MYSQLHOST,
     dialect: 'mysql',
-    port: 3306 || 3307
+    port: process.env.MYSQLPORT || 3307
 })
 
 module.exports = {
     Sequelize: Sequelize,
     sequelize: sequelize
 }
+*/
+require('dotenv').config();
+const Sequelize = require('sequelize');
 
+// Pegue a URL de conexão do MySQL do Railway
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'mysql',
+    protocol: 'mysql',
+    logging: false,  // Desativa os logs SQL, se necessário
+});
+
+module.exports = {
+  Sequelize,
+  sequelize
+};
