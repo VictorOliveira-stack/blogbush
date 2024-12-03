e972260b47ba64dd2c7b58230b9c60f0015bf2ec
Victor Oliveira <victormanoliv@gmail.com>
2024-12-03 12:13:31 -0300

    sql

--- models/db.js
+++ models/db.js
@@ -50,7 +50,7 @@ module.exports = {
 require('dotenv').config();
 const { Sequelize } = require('sequelize'); 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL , {
+const sequelize = new Sequelize(process.env.MySQL_nNil , {
     dialect: 'mysql',
     protocol: 'mysql',
     ssl: true,  // Railway exige SSL para a conexão
