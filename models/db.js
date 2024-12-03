8df053418b8c591ddc8322a1f4b6534e033f84e3
Victor Oliveira <victormanoliv@gmail.com>
2024-12-03 02:34:43 -0300

    const { Sequelize } = require('sequelize');

--- models/db.js
+++ models/db.js
@@ -47,6 +47,7 @@ require('dotenv').config();
        // }
    // }
 //});
+const { Sequelize } = require('sequelize'); 
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
