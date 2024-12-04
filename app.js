4351ba4b58962651467bc2e439a391284e9de81c
Victor Oliveira <victormanoliv@gmail.com>
2024-12-04 12:35:09 -0300

    no POST

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Express = require('express')
 const app = Express()
-const PORT = process.env.PORT || 3000 || 8080
+//const PORT = process.env.PORT || 3000 || 8080
 
 app.get('/' , function(req, res){
     return res.send('ola!')
 })
 
-app.listen(PORT, "0.0.0.0", function(){
+app.listen(3000 || 8080, "0.0.0.0", function(){
     console.log('rodando')
 })
