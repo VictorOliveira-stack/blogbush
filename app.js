2d6d7a1d8fc18cf6d60de196c9d866a75acaae2f
Victor Oliveira <victormanoliv@gmail.com>
2024-12-04 13:53:55 -0300

    sobre

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = Express()
 //const PORT = process.env.PORT || 3000 || 8080
 
 app.get('/' , function(req, res){
-    return res.render('feed.handlebars')
+    return res.render('sobre.html')
 })
 
 app.listen(3000 || 8080, "0.0.0.0", function(){
