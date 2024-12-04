0e7d4e1153410d92f025020be376a28b55a482c2
Victor Oliveira <victormanoliv@gmail.com>
2024-12-04 14:03:53 -0300

    voltou tudo

---
+++ config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+module.exports = {
+    "development": {
+        "password": process.env.B_senha1  //process.env.P_password
+    }
+    
+}
---
+++ models/post.js
@@ -0,0 +1,31 @@
+const db = require ('./db')
+
+const Post = db.sequelize.define('blogbush4s',{
+    titulo: {
+        type: db.Sequelize.STRING
+
+    },
+    conteudo: {
+    type: db.Sequelize.TEXT
+},
+    url: {
+        type: db.Sequelize.TEXT
+    }
+})
+
+/*Post.create({
+    titulo: 'Exemplo de Título',
+    conteudo: 'Aqui está o conteúdo do post.'
+})
+    .then(post => {
+        console.log('Post criado:', post);
+    })
+    .catch(error => {
+        console.error('Erro ao criar post:', error);
+    });
+
+Post.sync({force: true})
+
+module.exports = Post
+
+*/
