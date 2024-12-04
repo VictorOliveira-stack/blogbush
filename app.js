ea593bf1ec98eab84af570aa921b3b0b12bfad05
Victor Oliveira <victormanoliv@gmail.com>
2024-12-04 12:25:47 -0300

    "0.0.0.0"

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const Express = require('express')
 const app = Express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.get('/' , function(req, res){
     return res.send('ola!')
 })
 
-app.listen(PORT || 3000, function(){
+app.listen(PORT, "0.0.0.0", function(){
     console.log('rodando')
 })
