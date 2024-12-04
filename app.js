0b56cdd26dd230d03e11bacfa9a8a618c4c9cd4f
Victor Oliveira <victormanoliv@gmail.com>
2024-12-04 13:49:48 -0300

    render

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = Express()
 //const PORT = process.env.PORT || 3000 || 8080
 
 app.get('/' , function(req, res){
-    return res.send('ola!')
+    return res.render('feed.handlebars')
 })
 
 app.listen(3000 || 8080, "0.0.0.0", function(){
